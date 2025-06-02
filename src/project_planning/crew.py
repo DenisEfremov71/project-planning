67a9c70325c2a12466caf0b0347a311cd9b8bac2
Denis Efremov <denis.efremov@freightera.com>
2025-06-01 19:29:05 -0700

    Completed setup for Agents and Tasks

--- src/project_planning/crew.py
+++ src/project_planning/crew.py
@@ -2,12 +2,14 @@ from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
 from crewai.agents.agent_builder.base_agent import BaseAgent
 from typing import List
+from .models import ProjectPlan
 # If you want to run a snippet of code before or after the crew starts,
 # you can use the @before_kickoff and @after_kickoff decorators
 # https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
 
+
 @CrewBase
-class ProjectPlanning():
+class ProjectPlanning:
     """ProjectPlanning crew"""
 
     agents: List[BaseAgent]
@@ -16,37 +18,36 @@ class ProjectPlanning():
     # Learn more about YAML configuration files here:
     # Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
     # Tasks: https://docs.crewai.com/concepts/tasks#yaml-configuration-recommended
-    
+
     # If you would like to add tools to your agents, you can learn more about it here:
     # https://docs.crewai.com/concepts/agents#agent-tools
     @agent
-    def researcher(self) -> Agent:
-        return Agent(
-            config=self.agents_config['researcher'], # type: ignore[index]
-            verbose=True
-        )
+    def project_planning_agent(self) -> Agent:
+        return Agent(config=self.agents_config["project_planning_agent"])
 
     @agent
-    def reporting_analyst(self) -> Agent:
-        return Agent(
-            config=self.agents_config['reporting_analyst'], # type: ignore[index]
-            verbose=True
-        )
+    def estimation_agent(self) -> Agent:
+        return Agent(config=self.agents_config["estimation_agent"])
+
+    @agent
+    def resource_allocation_agent(self) -> Agent:
+        return Agent(config=self.agents_config["resource_allocation_agent"])
 
     # To learn more about structured task outputs,
     # task dependencies, and task callbacks, check out the documentation:
     # https://docs.crewai.com/concepts/tasks#overview-of-a-task
     @task
-    def research_task(self) -> Task:
-        return Task(
-            config=self.tasks_config['research_task'], # type: ignore[index]
-        )
+    def task_breakdown(self) -> Task:
+        return Task(config=self.tasks_config["task_breakdown"])
 
     @task
-    def reporting_task(self) -> Task:
+    def time_resource_estimation(self) -> Task:
+        return Task(config=self.tasks_config["time_resource_estimation"])
+
+    @task
+    def resource_allocation(self) -> Task:
         return Task(
-            config=self.tasks_config['reporting_task'], # type: ignore[index]
-            output_file='report.md'
+            config=self.tasks_config["resource_allocation"], output_pydantic=ProjectPlan
         )
 
     @crew
@@ -56,8 +57,8 @@ class ProjectPlanning():
         # https://docs.crewai.com/concepts/knowledge#what-is-knowledge
 
         return Crew(
-            agents=self.agents, # Automatically created by the @agent decorator
-            tasks=self.tasks, # Automatically created by the @task decorator
+            agents=self.agents,  # Automatically created by the @agent decorator
+            tasks=self.tasks,  # Automatically created by the @task decorator
             process=Process.sequential,
             verbose=True,
             # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
--- src/project_planning/main.py
+++ src/project_planning/main.py
@@ -13,15 +13,43 @@ warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
 # Replace with inputs you want to test with, it will automatically
 # interpolate any tasks and agents information
 
+
 def run():
     """
     Run the crew.
     """
+
+    project = "Website"
+    industry = "Technology"
+    project_objectives = "Create a website for a small business"
+    team_members = """
+    - John Doe (Project Manager)
+    - Jane Doe (Software Engineer)
+    - Bob Smith (Designer)
+    - Alice Johnson (QA Engineer)
+    - Tom Brown (QA Engineer)
+    """
+    project_requirements = """
+    - Create a responsive design that works well on desktop and mobile devices
+    - Implement a modern, visually appealing user interface with a clean look
+    - Develop a user-friendly navigation system with intuitive menu structure
+    - Include an "About Us" page highlighting the company's history and values
+    - Design a "Services" page showcasing the business's offerings with descriptions
+    - Create a "Contact Us" page with a form and integrated map for communication
+    - Implement a blog section for sharing industry news and company updates
+    - Ensure fast loading times and optimize for search engines (SEO)
+    - Integrate social media links and sharing capabilities
+    - Include a testimonials section to showcase customer feedback and build trust
+    """
+
     inputs = {
-        'topic': 'AI LLMs',
-        'current_year': str(datetime.now().year)
+        "project_type": project,
+        "industry": industry,
+        "project_objectives": project_objectives,
+        "team_members": team_members,
+        "project_requirements": project_requirements,
     }
-    
+
     try:
         ProjectPlanning().crew().kickoff(inputs=inputs)
     except Exception as e:
@@ -32,15 +60,15 @@ def train():
     """
     Train the crew for a given number of iterations.
     """
-    inputs = {
-        "topic": "AI LLMs",
-        'current_year': str(datetime.now().year)
-    }
+    inputs = {"topic": "AI LLMs", "current_year": str(datetime.now().year)}
     try:
-        ProjectPlanning().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
+        ProjectPlanning().crew().train(
+            n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs
+        )
 
     except Exception as e:
         raise Exception(f"An error occurred while training the crew: {e}")
+
 
 def replay():
     """
@@ -52,17 +80,17 @@ def replay():
     except Exception as e:
         raise Exception(f"An error occurred while replaying the crew: {e}")
 
+
 def test():
     """
     Test the crew execution and returns the results.
     """
-    inputs = {
-        "topic": "AI LLMs",
-        "current_year": str(datetime.now().year)
-    }
-    
+    inputs = {"topic": "AI LLMs", "current_year": str(datetime.now().year)}
+
     try:
-        ProjectPlanning().crew().test(n_iterations=int(sys.argv[1]), eval_llm=sys.argv[2], inputs=inputs)
+        ProjectPlanning().crew().test(
+            n_iterations=int(sys.argv[1]), eval_llm=sys.argv[2], inputs=inputs
+        )
 
     except Exception as e:
         raise Exception(f"An error occurred while testing the crew: {e}")
